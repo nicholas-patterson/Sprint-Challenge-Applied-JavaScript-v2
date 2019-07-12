0699c45c612e7a08ec81803df7ea52b1e2d4d6ae
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-07-12 11:36:03 -0400

    header.js complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(Header());
+
+function Header() {
+  // create elements
+  const header = document.createElement("div");
+  const headerDate = document.createElement("span");
+  const headerTitle = document.createElement("h1");
+  const headerTemp = document.createElement("span");
+
+  // add classes
+  header.classList.add("header");
+  headerDate.classList.add("date");
+  headerTemp.classList.add("temp");
+
+  // setting content
+  headerDate.textContent = "March 28, 2019";
+  headerTitle.textContent = "Lambda Times";
+  headerTemp.textContent = 98;
+
+  //append together
+  header.appendChild(headerDate);
+  header.appendChild(headerTitle);
+  header.appendChild(headerTemp);
+
+  return header;
+}
